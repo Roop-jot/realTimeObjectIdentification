2736f7b289e2b912d19e9784bcb89f12de47ded9
Roop-jot <100293679+Roop-jot@users.noreply.github.com>
2023-04-24 18:46:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ function gotResult(error, results)
 	  var utterThis = new SpeechSynthesisUtterance(speak_data);
 	  synth.speak(utterThis);
 	  document.getElementById("result_object_name").innerHTML = results[0].label;
-	  document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed[3];
+	  document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
     }
   }
 }
